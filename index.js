7ad3758d148cc3282e59c2a200aa8992108e5af8
ayerest <irisiseve@gmail.com>
2019-08-25 13:58:12 -0700

    finished js fundamentals arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1 
+
+let mod = 14 % 5
+
+let max = Math.max(1, 15, 20, 5, 6, 19)
