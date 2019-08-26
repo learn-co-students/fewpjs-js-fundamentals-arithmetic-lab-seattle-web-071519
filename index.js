fdc6d88e19be981debd9ea522a698748cb63b933
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-26 11:37:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+var multiply = num1 * num2
+
+let random = Math.random(300)
+
+num1 = 14
+num2 = 5
+
+var mod = num1 % num2
+
+var max = Math.max(1,2,3,15,20,19,18,4)
